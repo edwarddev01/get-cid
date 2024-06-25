18490bc740eb5fa49a5feb189b29afc10d563019
edwarddev01 <142266601+edwarddev01@users.noreply.github.com>
2024-06-24 23:03:17 -0500

    Update config.js

--- database/config.js
+++ database/config.js
@@ -11,14 +11,14 @@ const config = {
     timezone: "-05:00",
   },
   production: {
-    host: "65.21.238.170",
-    database: "adminedw_get_cid",
-    username: "adminedw_admincid",
+    host: "5.161.77.171",
+    database: "get_cid",
+    username: "admincid",
     password: "Lorica2021+*",
     port: 3306,
     dialect: "mysql",
     logging: false,
     timezone: "-05:00"
-  },
+  ,
 };
 export { config };
