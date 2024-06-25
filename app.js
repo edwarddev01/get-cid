d0d2cf436784d2e8e4eee614bb5b98f4d92f041c
edwarddev01 <142266601+edwarddev01@users.noreply.github.com>
2024-06-24 23:35:39 -0500

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(clientRouter);
 app.use(adminRouter);
 
 //Inicializamos el servidor y la conexion a la base de datos
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3100;
 app.listen(port, async () => {
     try {
         console.log(`Servidor inicializado en el puerto: ${port}`);
