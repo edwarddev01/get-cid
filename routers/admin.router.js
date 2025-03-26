b1f53be3afe8c52787477d18724129bc8d3b82de
Edward Díaz <dev@moshipp.com>
2025-03-26 08:42:09 -0500

    actualizar main

--- routers/admin.router.js
+++
@@ -1,9 +0,0 @@
-//Importamos paquetes y archivos
-import { Router } from "express";
-import { adminController } from "../controllers/admin.controller.js";
-import { validateToken } from "../utils/token.util.js";
-export const adminRouter = Router();
-
-adminRouter.post("/api/v1/admin", adminController.createAdmin);
-adminRouter.post("/api/v1/admin/login", adminController.loginAdmin);
-adminRouter.get("/api/v1/admin/token",[validateToken],adminController.getToken)
--- routers/client.router.js
+++
@@ -1,9 +0,0 @@
-//Importamos paquetes y archivos
-import { Router } from "express";
-import { clientController } from "../controllers/client.controller.js";
-import { verifyRecaptchaToken } from "../utils/recaptcha.util.js";
-import { tokenValido } from "../utils/token.util.js";
-export const clientRouter = Router();
-
-clientRouter.post("/api/v1/get-cid",[verifyRecaptchaToken, tokenValido], clientController.getCID);
-
