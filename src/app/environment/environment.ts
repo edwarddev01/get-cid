ae2d926fe00c59c1183729b4fd8d085458b776ee
edwarddev01 <dev@moshipp.com>
2025-03-26 17:58:34 -0500

    Actualizacion

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,2 +1,2 @@
-//export const apiUrl = "https://apigetcid.licenciadigital.com.co/api/v1"
-export const apiUrl = "http://localhost:3100/api/v1"
+export const apiUrl = "https://apigetcid.licenciadigital.com.co/api/v1"
+//export const apiUrl = "http://localhost:3100/api/v1"
